f9e2205a623069fcbd4ba509bcb620fb74ed7947
Daniel Lynch <113555581+DanielLynch1121@users.noreply.github.com>
2023-11-16 22:05:58 -0500

    changes to GUI

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.MenuPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -35,19 +36,21 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.ExportFile = new System.Windows.Forms.Button();
             this.ClearTable = new System.Windows.Forms.Button();
+            this.loadinganimationgif = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.MenuPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.loadinganimationgif)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.dataGridView1.Location = new System.Drawing.Point(288, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGridView1.Location = new System.Drawing.Point(143, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
-            this.dataGridView1.Size = new System.Drawing.Size(1706, 812);
+            this.dataGridView1.Size = new System.Drawing.Size(778, 422);
             this.dataGridView1.TabIndex = 4;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
@@ -60,19 +63,19 @@
             this.MenuPanel.Controls.Add(this.ExportFile);
             this.MenuPanel.Controls.Add(this.ClearTable);
             this.MenuPanel.Location = new System.Drawing.Point(0, 0);
-            this.MenuPanel.Margin = new System.Windows.Forms.Padding(6);
             this.MenuPanel.Name = "MenuPanel";
-            this.MenuPanel.Size = new System.Drawing.Size(286, 812);
+            this.MenuPanel.Size = new System.Drawing.Size(143, 422);
             this.MenuPanel.TabIndex = 6;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Controls.Add(this.loadinganimationgif);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(302, 129);
+            this.panel1.Size = new System.Drawing.Size(151, 67);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -82,23 +85,30 @@
             this.EnterNumber.FlatAppearance.BorderSize = 0;
             this.EnterNumber.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.EnterNumber.ForeColor = System.Drawing.Color.Gainsboro;
-            this.EnterNumber.Location = new System.Drawing.Point(0, 129);
+            this.EnterNumber.Location = new System.Drawing.Point(0, 67);
             this.EnterNumber.Margin = new System.Windows.Forms.Padding(0);
             this.EnterNumber.Name = "EnterNumber";
-            this.EnterNumber.Size = new System.Drawing.Size(286, 77);
+            this.EnterNumber.Size = new System.Drawing.Size(143, 40);
             this.EnterNumber.TabIndex = 2;
             this.EnterNumber.Text = "Run Simulation";
             this.EnterNumber.UseVisualStyleBackColor = true;
             this.EnterNumber.Click += new System.EventHandler(this.EnterNumber_Click);
+            this.EnterNumber.KeyDown += new System.Windows.Forms.KeyEventHandler(this.EnterNumber_KeyDown);
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.LightGray;
-            this.textBox1.Location = new System.Drawing.Point(0, 206);
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textBox1.Location = new System.Drawing.Point(0, 107);
             this.textBox1.Margin = new System.Windows.Forms.Padding(0);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(282, 31);
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
             this.textBox1.TabIndex = 3;
+            this.textBox1.Text = " ";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // ExportFile
             // 
@@ -106,10 +116,10 @@
             this.ExportFile.FlatAppearance.BorderSize = 0;
             this.ExportFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ExportFile.ForeColor = System.Drawing.Color.Gainsboro;
-            this.ExportFile.Location = new System.Drawing.Point(0, 237);
+            this.ExportFile.Location = new System.Drawing.Point(0, 127);
             this.ExportFile.Margin = new System.Windows.Forms.Padding(0);
             this.ExportFile.Name = "ExportFile";
-            this.ExportFile.Size = new System.Drawing.Size(286, 77);
+            this.ExportFile.Size = new System.Drawing.Size(143, 40);
             this.ExportFile.TabIndex = 4;
             this.ExportFile.Text = "Export file\r\n";
             this.ExportFile.UseVisualStyleBackColor = true;
@@ -121,29 +131,48 @@
             this.ClearTable.FlatAppearance.BorderSize = 0;
             this.ClearTable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ClearTable.ForeColor = System.Drawing.Color.Gainsboro;
-            this.ClearTable.Location = new System.Drawing.Point(0, 314);
+            this.ClearTable.Location = new System.Drawing.Point(0, 167);
             this.ClearTable.Margin = new System.Windows.Forms.Padding(0);
             this.ClearTable.Name = "ClearTable";
-            this.ClearTable.Size = new System.Drawing.Size(286, 77);
+            this.ClearTable.Size = new System.Drawing.Size(143, 40);
             this.ClearTable.TabIndex = 5;
             this.ClearTable.Text = "Clear Table";
             this.ClearTable.UseVisualStyleBackColor = true;
             this.ClearTable.Click += new System.EventHandler(this.ClearTable_Click);
             // 
+            // loadinganimationgif
+            // 
+            this.loadinganimationgif.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loadinganimationgif.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.loadinganimationgif.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.loadinganimationgif.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.loadinganimationgif.Image = ((System.Drawing.Image)(resources.GetObject("loadinganimationgif.Image")));
+            this.loadinganimationgif.Location = new System.Drawing.Point(-52, 0);
+            this.loadinganimationgif.Margin = new System.Windows.Forms.Padding(0);
+            this.loadinganimationgif.Name = "loadinganimationgif";
+            this.loadinganimationgif.Size = new System.Drawing.Size(216, 67);
+            this.loadinganimationgif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.loadinganimationgif.TabIndex = 1;
+            this.loadinganimationgif.TabStop = false;
+            // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1994, 812);
+            this.ClientSize = new System.Drawing.Size(921, 422);
             this.Controls.Add(this.MenuPanel);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.Text = "Sim. Results";
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.MenuPanel.ResumeLayout(false);
             this.MenuPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.loadinganimationgif)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -156,5 +185,6 @@
         private System.Windows.Forms.Button EnterNumber;
         private System.Windows.Forms.Button ExportFile;
         private System.Windows.Forms.Button ClearTable;
+        public System.Windows.Forms.PictureBox loadinganimationgif;
     }
 }
